75e87869b746f7a0a804fd16231df10fed2f6576
Aziz Khan <sakatazizkhan1@gmail.com>
2024-08-24 16:54:28 +0530

    updated tsconfig

--- resources/js/Pages/Welcome.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import { Head } from '@inertiajs/react';
-import { LandingComponent } from './Home/landing-component';
-export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    return (_jsxs(_Fragment, { children: [_jsx(Head, { title: "S.Aziz Khan" }), _jsx(LandingComponent, {})] }));
-}
