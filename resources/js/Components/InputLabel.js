fea5ead85e471f5a8fbdaf05c39a191a7ac64065
Aziz Khan <sakatazizkhan1@gmail.com>
2024-08-24 17:17:10 +0530

    remove unwanted JS

--- resources/js/Components/InputLabel.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function InputLabel({ value, className = '', children, ...props }) {
-    return (_jsx("label", { ...props, className: `block font-medium text-sm text-gray-700 ` + className, children: value ? value : children }));
-}
