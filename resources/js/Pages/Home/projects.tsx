9e98f9d9d8bdce9943edb99fa47e1f95635f2c31
Aziz Khan <sakatazizkhan1@gmail.com>
2024-08-26 08:34:49 +0530

    projects DATA updated

--- resources/js/Pages/Home/projects.tsx
+++ resources/js/Pages/Home/projects.tsx
@@ -52,22 +52,24 @@ const ProjectCard = (project: ProjectInterface) => {
             <CardFooter>
                 <div className="flex gap-2">
                     {project.previewLink && (
-                        <Link
+                        <a
+                            target="_blank"
                             href={project.previewLink}
                             className="inline-flex h-8 items-center justify-center rounded-md bg-primary px-4 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                         >
                             Live Demo
                             <EyeIcon className="ml-2 w-5 h-5" />
-                        </Link>
+                        </a>
                     )}
                     {project.githubLink && (
-                        <Link
+                        <a
+                            target="_blank"
                             href={project.githubLink}
                             className="inline-flex h-8 items-center justify-center rounded-md border border-input bg-background px-4 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                         >
                             GitHub
                             <GithubIcon className="ml-2 w-5 h-5" />
-                        </Link>
+                        </a>
                     )}
                 </div>
             </CardFooter>
